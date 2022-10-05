7673484bd8eba74144ae542e9fa2d7ad0ead24ed
musab174444@gmail.com <1744440musab>
2022-10-05 17:07:24 +0200

    Testing exercise

--- tests/integration/test_diffusion2d.py
+++ tests/integration/test_diffusion2d.py
@@ -3,6 +3,8 @@ Tests for functionality checks in class SolveDiffusion2D
 """
 
 from diffusion2d import SolveDiffusion2D
+import pytest
+import numpy as np
 
 
 def test_initialize_physical_parameters():
@@ -10,10 +12,19 @@ def test_initialize_physical_parameters():
     Checks function SolveDiffusion2D.initialize_domain
     """
     solver = SolveDiffusion2D()
+    solver.initialize_domain(20., 20., 1., 1.)
+    solver.initialize_physical_parameters(5., 200., 500.)
+    expected_dt = pytest.approx(0.05, rel=1e-2)
+    assert solver.dt == expected_dt
 
 
 def test_set_initial_condition():
     """
-    Checks function SolveDiffusion2D.get_initial_function
+    Checks function SolveDiffusion2D.get_initial_condition
     """
     solver = SolveDiffusion2D()
+    solver.initialize_domain(30., 30., 10., 10.)
+    solver.initialize_physical_parameters(5., 200., 500.)
+    u0 = solver.set_initial_condition()
+    expected_u0 = pytest.approx(np.array([[200., 200., 200.],[200., 200., 200.],[200., 200., 200.]]), rel=1e-2)
+    assert u0 == expected_u0
--- tests/unit/test_diffusion2d_functions.py
+++ tests/unit/test_diffusion2d_functions.py
@@ -1,26 +1,63 @@
 """
 Tests for functions in class SolveDiffusion2D
 """
-
+import pytest
+import numpy as np
 from diffusion2d import SolveDiffusion2D
+from unittest import TestCase
+
+class TestDiffusion2D(TestCase):
+    def setUp(self):
+        # plate size, mm
+        self.w = 20.
+        self.h = 20.
+
+        # intervals in x-, y- directions, mm
+        self.dx = 1.
+        self.dy = 1.
+
+        # Thermal diffusivity of steel, mm^2/s
+        self.D = 5.
+
+        # Initial cold temperature of square domain
+        self.T_cold = 200.
+
+        # Initial hot temperature of circular disc at the center
+        self.T_hot = 500.
+
+    def test_initialize_domain(self):
+        """
+        Check function SolveDiffusion2D.initialize_domain
+        """
+        solver = SolveDiffusion2D()
+        solver.initialize_domain(self.w, self.h, self.dx, self.dy)
+        self.assertEqual(solver.nx, 20)
+        self.assertEqual(solver.ny, 20)
 
 
-def test_initialize_domain():
-    """
-    Check function SolveDiffusion2D.initialize_domain
-    """
-    solver = SolveDiffusion2D()
+    def test_initialize_physical_parameters(self):
+        """
+        Checks function SolveDiffusion2D.initialize_domain
+        """
+        solver = SolveDiffusion2D()
+        solver.dx = 1.
+        solver.dy = 1.
+        solver.initialize_physical_parameters(self.D, self.T_cold, self.T_hot)
+        expected_dt = 0.05
+        self.assertAlmostEqual(solver.dt, expected_dt, 2)
 
 
-def test_initialize_physical_parameters():
-    """
-    Checks function SolveDiffusion2D.initialize_domain
-    """
-    solver = SolveDiffusion2D()
-
-
-def test_set_initial_condition():
-    """
-    Checks function SolveDiffusion2D.get_initial_function
-    """
-    solver = SolveDiffusion2D()
+    def test_set_initial_condition(self):
+        """
+        Checks function SolveDiffusion2D.get_initial_function
+        """
+        solver = SolveDiffusion2D()
+        solver.dx = 10.
+        solver.dy = 10.
+        solver.T_cold = 200.
+        solver.T_hot = 500.
+        solver.nx = 3
+        solver.ny = 3
+        u0 = solver.set_initial_condition()
+        expected_u0 = np.array([[200., 200., 200.],[200., 200., 200.],[200., 200., 200.]])
+        self.assertAlmostEqual(u0[0,0], expected_u0[0,0], 3)
