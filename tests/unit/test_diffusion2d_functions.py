c2c49d575defd8463c67226a52a2d016a6b41905
Fabian Kannenberg <fabian.kannenberg@icd.uni-stuttgart.de>
2024-10-08 17:06:32 +0200

    fix docstrings

--- tests/unit/test_diffusion2d_functions.py
+++ tests/unit/test_diffusion2d_functions.py
@@ -14,13 +14,13 @@ def test_initialize_domain():
 
 def test_initialize_physical_parameters():
     """
-    Checks function SolveDiffusion2D.initialize_domain
+    Checks function SolveDiffusion2D.initialize_physical_parameters
     """
     solver = SolveDiffusion2D()
 
 
 def test_set_initial_condition():
     """
-    Checks function SolveDiffusion2D.get_initial_function
+    Checks function SolveDiffusion2D.set_initial_condition
     """
     solver = SolveDiffusion2D()
